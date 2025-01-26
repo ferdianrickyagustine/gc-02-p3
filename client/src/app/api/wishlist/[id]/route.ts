ae864376d36c7522c852096b22d2ba1721687d5e
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-01-26 20:08:41 +0700

    redeploy

--- client/src/app/api/wishlist/[id]/route.ts
+++ client/src/app/api/wishlist/[id]/route.ts
@@ -4,54 +4,44 @@ import { readPayloadJose } from "@/utils/jwt";
 import { ObjectId } from "mongodb";
 import { deleteWishlist } from "@/db/models/wishlist";
 
-interface RouteContext {
-  params: {
-      id: string;
-  };
-}
-
 export async function DELETE(
   request: NextRequest,
-  context: RouteContext
+  { params }: { params: { id: string } }
 ) {
   try {
-      const cookieStore = await cookies();
-      const token = cookieStore.get("token");
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
 
-      if (!token) {
-          return NextResponse.json(
-              { error: "Unauthorized" },
-              { status: 401 }
-          );
-      }
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
-      await readPayloadJose<{ id: string }>(token.value);
-      const result = await deleteWishlist(new ObjectId(context.params.id));
+    const result = await deleteWishlist(params.id);
 
-      if (result.deletedCount === 0) {
-          return NextResponse.json(
-              {
-                  statusCode: 404,
-                  error: "Wishlist item not found",
-              },
-              { status: 404 }
-          );
-      }
-
+    if (!result.deletedCount) {
       return NextResponse.json(
-          {
-              statusCode: 200,
-              message: "Wishlist item deleted successfully",
-          },
-          { status: 200 }
+        {
+          statusCode: 404,
+          error: "Wishlist item not found",
+        },
+        { status: 404 }
       );
+    }
+
+    return NextResponse.json(
+      {
+        statusCode: 200,
+        message: "Wishlist item deleted successfully",
+      },
+      { status: 200 }
+    );
   } catch {
-      return NextResponse.json(
-          {
-              statusCode: 500,
-              error: "Internal server error",
-          },
-          { status: 500 }
-      );
+    return NextResponse.json(
+      {
+        statusCode: 500,
+        error: "Internal server error",
+      },
+      { status: 500 }
+    );
   }
 }
