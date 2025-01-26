146baa1e923b3d4fd4db6eacbead21e5fd9109ca
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-01-26 16:04:46 +0700

    ready to deploy

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['img.lazcdn.com', 'placehold.co'],
+}
 };
 
 export default nextConfig;
