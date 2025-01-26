9172954dfbbd67ecf5565244f21f682654561e30
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-01-26 20:13:56 +0700

    redeploy

--- client/src/app/api/wishlist/[id]/route.ts
+++ client/src/app/api/wishlist/[id]/route.ts
@@ -2,10 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { deleteWishlist } from "@/db/models/wishlist";
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export async function DELETE(request: NextRequest, context: Props) {
   try {
     const cookieStore = await cookies();
     const token = cookieStore.get("token");
@@ -14,7 +17,7 @@ export async function DELETE(
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
-    const result = await deleteWishlist(params.id);
+    const result = await deleteWishlist(context.params.id);
 
     if (!result.deletedCount) {
       return NextResponse.json(
