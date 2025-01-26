0a4655548e207ef059f5732fe9786b8c6cb6328d
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-01-26 19:25:33 +0700

    a

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   images: {
     domains: ["img.lazcdn.com", "placehold.co"],
   },
